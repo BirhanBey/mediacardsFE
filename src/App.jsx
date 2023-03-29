0c7a8c10763be61fa6ce3592af83b8cdd95f40df
Noah Bovee <noahbov@gmail.com>
2023-03-29 16:34:49 +0200

    add-button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import ListArea from "./components/ListArea";
+import AddButton from "./components/AddButton";
 
 const App = () => {
   return (
     <div>
       <Header />
       <ListArea />
+      <AddButton />
     </div>
   );
 };
