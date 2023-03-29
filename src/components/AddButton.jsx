7957625af8cabcfd8696fc3a9710a91859ff961f
Noah Bovee <noahbov@gmail.com>
2023-03-29 18:17:47 +0200

    functional-add-button

--- src/components/AddButton.jsx
+++ src/components/AddButton.jsx
@@ -1,11 +1,59 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
-const AddButton = () => {
+const AddButton = ({ addCard }) => {
+  const [show, setShow] = useState(false);
+  const [link, setLink] = useState("");
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+  const handleLinkChange = (event) => setLink(event.target.value);
+
+  const handleSave = () => {
+    addCard(link);
+    setLink("");
+    handleClose();
+  };
+
   return (
-    <div>
-      <Button variant="primary">Primary</Button>{" "}
-    </div>
+    <>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <Button variant="primary" onClick={handleShow}>
+            Add Link
+          </Button>
+
+          <Modal show={show} onHide={handleClose}>
+            <Modal.Header closeButton>
+              <Modal.Title>New Link</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <form>
+                <label htmlFor="link">Link:</label>
+                <input
+                  type="text"
+                  id="link"
+                  name="link"
+                  value={link}
+                  onChange={handleLinkChange}
+                />
+              </form>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={handleClose}>
+                Close
+              </Button>
+              <Button variant="primary" onClick={handleSave}>
+                Save
+              </Button>
+            </Modal.Footer>
+          </Modal>
+        </Col>
+      </Row>
+    </>
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import "./index.css";
   crossorigin
 ></script>;
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
