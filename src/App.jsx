50bd12fe51f5e31598873dd5c6bde336e7c7a4a8
Noah Bovee <noahbov@gmail.com>
2023-03-30 12:03:23 +0200

    copyright-footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
 import ListArea from "./components/ListArea";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <div>
       <Header />
       <ListArea />
+      <Footer />
     </div>
   );
 };
