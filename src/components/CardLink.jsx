47481af84a8b9ab213dc5a59b1028fa06cc22e97
Noah Bovee <noahbov@gmail.com>
2023-04-06 12:11:32 +0200

    card-layout

--- src/components/CardLink.jsx
+++ src/components/CardLink.jsx
@@ -1,29 +1,39 @@
 import React, { useState } from "react";
-import { Col, Container, Row, Card } from "react-bootstrap";
+import { Col, Container, Row, Card, Modal } from "react-bootstrap";
 import axios from "axios";
 import DelButton from "./DelButton";
 
 const CardLink = ({ link, index, removeCard }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <Container>
       <Row>
         <Card
           className="d-flex flex-md-row align-items-sm-center"
           id="card-item"
+          onClick={toggleExpand}
+          style={{ cursor: "pointer", width: "700px", minHeight: "80px" }}
         >
-          <Col xs={3}>
+          {/* <Col xs={3}>
             <Card.Img
               variant="top"
               src="https://media.licdn.com/dms/image/C4E0BAQHw2mk1XF7vUw/company-logo_200_200/0/1672915872372?e=1688601600&v=beta&t=aNGanUg7_IGdNM6hJPtjHH6EttjV9snQzil_gm3X7m0"
             />
-          </Col>
+          </Col> */}
           <Col>
             <Card.Body>
               <Card.Title>{link.name}</Card.Title>
-              <Card.Text className="text-wrap">
-                {link.description || "No description available."}
-              </Card.Text>
-              <Card.Link href={link.url}>Website</Card.Link>
+              {expanded && (
+                <Card.Text className="text-wrap">
+                  {link.description || "No description available."}
+                </Card.Text>
+              )}
+              <Card.Link href={link.url}>Go to my {link.name}</Card.Link>
               <div className="d-flex justify-content-end">
                 <DelButton
                   index={index}
