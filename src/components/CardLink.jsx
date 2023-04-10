388359ea93637fbe8152531d196c1f45937a17a6
Noah Bovee <noahbov@gmail.com>
2023-04-10 11:51:06 +0200

    card-layout2

--- src/components/CardLink.jsx
+++ src/components/CardLink.jsx
@@ -24,28 +24,26 @@ const CardLink = ({ link, index, removeCard }) => {
             backgroundColor: "#212529",
           }}
         >
-          {/* <Col xs={3}>
-            <Card.Img
-              variant="top"
-              src="https://media.licdn.com/dms/image/C4E0BAQHw2mk1XF7vUw/company-logo_200_200/0/1672915872372?e=1688601600&v=beta&t=aNGanUg7_IGdNM6hJPtjHH6EttjV9snQzil_gm3X7m0"
-            />
-          </Col> */}
           <Col>
             <Card.Body>
               <Card.Title>{link.name}</Card.Title>
+
               {expanded && (
-                <Card.Text className="text-wrap">
-                  {link.description || "No description available."}
-                </Card.Text>
+                <div>
+                  {" "}
+                  <Card.Text className="text-wrap">
+                    {link.description || "No description available."}
+                  </Card.Text>
+                  <Card.Link href={link.url}>Go to my {link.name}</Card.Link>
+                  <div className="d-flex justify-content-end">
+                    <DelButton
+                      index={index}
+                      removeCard={removeCard}
+                      linkId={link.id}
+                    />
+                  </div>
+                </div>
               )}
-              <Card.Link href={link.url}>Go to my {link.name}</Card.Link>
-              <div className="d-flex justify-content-end">
-                <DelButton
-                  index={index}
-                  removeCard={removeCard}
-                  linkId={link.id}
-                />
-              </div>
             </Card.Body>
           </Col>
         </Card>
