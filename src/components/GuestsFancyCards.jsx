c68738d664381d11f3f4157982e1d4a9692381a9
Noah Bovee <114658571+NoahBovee@users.noreply.github.com>
2023-04-26 23:25:02 +0200

    quik card fix (#120)

--- src/components/GuestsFancyCards.jsx
+++ src/components/GuestsFancyCards.jsx
@@ -30,7 +30,10 @@ const FancyCards = ({
 
   return (
     <>
-      <Container className="d-flex justify-content-center" style={{maxWidth: "500px"}}>
+      <Container
+        className="d-flex justify-content-center"
+        style={{ maxWidth: "500px" }}
+      >
         <div
           className="flip-card"
           onClick={handleCardClick}
@@ -74,16 +77,29 @@ const FancyCards = ({
                 )}
               </div>
             </div>
-            <div className="flip-card-back">
+            <div
+              className="flip-card-back"
+              style={{ backgroundColor: darkMode ? "black" : "white" }}
+            >
+              <div className="icon-back">
+                <i className={`fa fa-${icon}`} aria-hidden="true"></i>
+              </div>
               <div
                 className="content w-100"
                 style={{
                   color: darkMode ? "#ff0000" : "black",
                 }}
               >
-                <h5 className="mt-3">{link.name}</h5>
-                {link.description || "No description available."}
-                <br />
+                <h5 className="mt-4">{link.name}</h5>
+                <div className="description" style={{ fontStyle: "italic" }}>
+                  {link.description
+                    ? link.description.slice(0, 20) +
+                      (link.description.length > 20
+                        ? "\n" + link.description.slice(20, 40)
+                        : "")
+                    : "No description available."}
+                </div>
+
                 <a href={link.link} target="_blank">
                   Go to {link.name}
                 </a>
@@ -91,58 +107,6 @@ const FancyCards = ({
             </div>
           </div>
         </div>
-        {/* <div className="card border-0 bg-transparent">
-          <div className="slide slide1 position-relative d-flex justify-content-center align-items-center">
-            <div
-              className="icon rounded"
-              onClick={() => handleIconClick(cardIcon)}
-              style={{ backgroundColor: `${theme}` }}
-            >
-              {cardIcon ? (
-                cardIcon
-              ) : icon !== "facebook" &&
-                icon !== "instagram" &&
-                icon !== "twitter" &&
-                icon !== "linkedin" &&
-                icon !== "youtube" &&
-                icon !== "tiktok" &&
-                icon !== "reddit" &&
-                icon !== "snapchat" &&
-                icon !== "github" &&
-                icon !== "slack" &&
-                icon !== "discord" &&
-                icon !== "spotify" &&
-                icon !== "apple" &&
-                icon !== "google" &&
-                icon !== "amazon" &&
-                icon !== "skype" &&
-                icon !== "zoom" &&
-                icon !== "soundcloud" ? (
-                <i className="fa fa-user" aria-hidden="true"></i>
-              ) : (
-                <i className={`fa fa-${icon}`} aria-hidden="true"></i>
-              )}
-            </div>
-          </div>
-          <div className="slide slide2 position-relative d-flex justify-content-center align-items-center p-20 rounded">
-            <div
-              className="content w-100"
-              style={{
-                color: darkMode ? "#ff0000" : "black",
-              }}
-            >
-              <h5>{link.name}</h5>
-              {link.description || "No description available."}
-              <br />
-              <a href={link.link} target="_blank">
-                Go to {link.name}
-              </a>
-              <div className="d-flex mt-3" id="card-buttons">
-
-              </div>
-            </div>
-          </div>
-        </div> */}
       </Container>
       <Footer />
     </>
