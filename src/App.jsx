81000d39e05e41a6d91706337b07ec9b452f8f3b
Marouan Ajtirah <marouan_39@hotmail.com>
2023-04-27 00:35:17 +0200

    fixedImg

--- src/App.jsx
+++ src/App.jsx
@@ -268,7 +268,15 @@ function App() {
                   }}
                 >
                   <Offcanvas.Title>
-                    <img style={{maxWidth: "300px", marginLeft: "30px", marginRight: "-30px"}} src="../public/Offcanvas.png" alt="Page Name"/>
+                    <img
+                      style={{
+                        maxWidth: "300px",
+                        marginLeft: "30px",
+                        marginRight: "-30px",
+                      }}
+                      src="./Offcanvas.png"
+                      alt="Page Name"
+                    />
                   </Offcanvas.Title>
                 </Offcanvas.Header>
                 <Offcanvas.Body>
@@ -316,7 +324,9 @@ function App() {
                       variant={darkMode ? "secondary" : "dark"}
                       onClick={handleLogout}
                       className="mt-auto mb-5"
-                      style={{border: darkMode ? "1px solid white" : "1px solid grey"}}
+                      style={{
+                        border: darkMode ? "1px solid white" : "1px solid grey",
+                      }}
                     >
                       Logout
                     </Button>
